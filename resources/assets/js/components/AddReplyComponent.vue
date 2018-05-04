af0a8295e3e5ec1f39eefc1a2d232421048f5e1d
Eliott Champault <eliott@shm-ltd.co.uk>
2018-05-04 13:18:35 +0100

    github sync

--- resources/assets/js/components/AddReplyComponent.vue
+++ resources/assets/js/components/AddReplyComponent.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div v-if="signedIn">
-			<p class="mb-2 text-center semiBold">Leave a reply please</p>
+			<p class="mb-2 text-center semiBold">Leave a reply again</p>
             <form @submit.prevent="addReply">
                 <div class="form-group">
                     <label class="f-xs">Please provide a link to YouTube</label>
