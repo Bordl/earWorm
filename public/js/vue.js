eeed617d728d2280c1d86a9ea3fc5d57d836d5dc
Eliott Champault <eliott@shm-ltd.co.uk>
2018-05-07 02:20:07 +0100

    reply v2

--- public/js/vue.js
+++ public/js/vue.js
@@ -15118,7 +15118,7 @@ exports = module.exports = __webpack_require__(3)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -15131,49 +15131,6 @@ exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__PostComponent__ = __webpack_require__(184);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__PostComponent___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__PostComponent__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
@@ -15756,12 +15713,14 @@ var render = function() {
   return _c(
     "div",
     _vm._l(_vm.dataSet, function(post) {
-      return _c(
-        "div",
-        { key: post.id },
-        [_c("post-component", { attrs: { data: post } })],
-        1
-      )
+      return _vm.dataSet
+        ? _c(
+            "div",
+            { key: post.id },
+            [_c("post-component", { attrs: { data: post } })],
+            1
+          )
+        : _vm._e()
     })
   )
 }
--- resources/assets/js/components/PostsComponent.vue
+++ resources/assets/js/components/PostsComponent.vue
@@ -1,49 +1,6 @@
 <template>
     <div>
-        <div v-cloak v-for="post in dataSet" :key="post.id">
-            <!-- <div class="card mb-2 mt-2">
-                <div class="card-body">
-                    <div class="level">
-                        <a href="#" class="flex semiBold">
-                            <div v-text="post.creator.name"></div>
-                        </a>
-
-                        <div class="f-xs"><em v-text="moment(post.created_at)"></em>...</div>
-                    </div>
-                    
-                    <hr>
-
-                    <div class="row level mb-3">
-                        <div class="col-8">
-                            <h5>What's this song?</h5>
-                            <p v-if="post.description" class="f-xs m-0">
-                                {{ post.description }}
-                            </p>
-
-                            <p v-else class="f-xs m-0">This post has no description.</p>
-                        </div>
-
-                        <div class="col-4">
-                            <player-component :path="post.recording.path" :postID="post.id"></player-component>
-                        </div>
-                    </div>
-
-                    <hr class="mt-1 mb-2">
-
-                    <div>
-                        <router-link :to="`/posts/${post.id}`">
-                            <p class="mb-1">
-                                <span><i class="far fa-comment-alt"></i></span>
-                                &nbsp;<span v-text="this.comments"></span>  
-                            </p>
-                        </router-link>
-                    </div>
-                </div>
-            </div> -->
-
-
-
-
+        <div v-cloak v-if="dataSet" v-for="post in dataSet" :key="post.id">
             <post-component :data="post"></post-component>
         </div>
     </div>
