3c0606c6766ac953a4120fdc292252c3c61acf9c
Eliott Champault <eliott@shm-ltd.co.uk>
2018-05-15 18:41:26 +0100

    following test

--- app/User.php
+++ app/User.php
@@ -109,7 +109,7 @@ class User extends Authenticatable
 
     public function following()
     {
-        dd($this->followUser()->where('follower_id', $this->id)->get());
+        dd($this->followUser()->where('follower_id', '!=', '6')->get());
         return $this->followUser()->where('follower_id', $this->id)->get();
     }
 }
